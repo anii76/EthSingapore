9d9eb79a04c6cf578ba1ec16402ddf3917608bed
Kalzak <kalzak.dev@protonmail.com>
2024-09-22 03:29:26 +0800

    quick cleanup

--- backend/resolve_ens.py
+++ backend/resolve_ens.py
@@ -2,11 +2,9 @@ from web3 import Web3
 
 INFURA_URL = "https://rpc.ankr.com/eth"
 
-
 def get_rpc_provider():
     # Initialize a Web3 instance
     return Web3(Web3.HTTPProvider(INFURA_URL))
-
 
 def get_address_from_ens(ens_name):
     # Replace with your own Infura or Alchemy endpoint or any other Ethereum node provider.
@@ -15,5 +13,4 @@ def get_address_from_ens(ens_name):
     if not w3.is_connected():
         print("Unable to connect to the Ethereum network.")
         exit(1)
-
     return w3.ens.address(ens_name)
